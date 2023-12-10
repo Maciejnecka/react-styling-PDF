c6b836755ebc2a2ae7e35d656802d0484031801a
Maciej Necka <macieknecka@gmail.com>
2023-12-10 20:57:38 +0100

    #08 Theme / 03. Styled Components

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@
 // ./src/components/App.js
 import React from 'react';
 
-import Box from './Box';
 import ResetStyle from './styled/Reset';
 import GlobalStyle from './styled/Global';
+import LoginPanel from './LoginPanel';
 
 const App = () => (
     <>
         <ResetStyle />
         <GlobalStyle />
-        <Box />
+        <LoginPanel />
     </>
 );
 
