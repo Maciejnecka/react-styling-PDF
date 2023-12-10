9fa6370168d89555d931be3582b212280d5adf05
Maciej Necka <macieknecka@gmail.com>
2023-12-10 15:59:04 +0100

    #03 props / 03. Styled Components

--- src/components/Div.js
+++ src/components/Div.js
@@ -1,3 +1,8 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/prop-types */
 // import React from 'react';
 
@@ -18,19 +23,29 @@
 // }
 
 import React from 'react';
-
-function Div(props) {
-    const { style = {}, type } = props;
-    const bgColor = type === 'dark' ? '#c5aeb4' : '#fce2c2';
-
-    const defaultStyle = {
-        '--color-alfa': '#6cb2d1',
-        backgroundColor: bgColor,
-        border: '2px solid var(--color-alfa)',
-        color: 'var(--color-alfa)',
-        padding: '20px',
-    };
-    return <div style={{ ...defaultStyle, ...style }}>React: Styling</div>;
-}
-
+import styled from 'styled-components';
+const StyledDiv = styled.div`
+    --color-alfa: #6cb2d1;
+    background-color: #fce2c2;
+    border: 2px solid var(--color-alfa);
+    color: var(--color-alfa);
+    padding: 20px;
+    &:hover {
+        background-color: #fcc5c2;
+    }
+    h1 {
+        font-size: 20px;
+    }
+    p {
+        font-size: 12px;
+    }
+`;
+const Div = ({ style = {}, type }) => {
+    return (
+        <StyledDiv>
+            <h1>React: Styled Components</h1>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </StyledDiv>
+    );
+};
 export default Div;
