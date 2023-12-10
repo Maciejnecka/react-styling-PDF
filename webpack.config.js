13c5715de2242c0217896a616bfb78d6b2bf44a3
Maciej Necka <macieknecka@gmail.com>
2023-12-10 16:15:25 +0100

    webpack fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,66 @@
 /* eslint-disable no-undef */
-/* eslint-disable no-unneeded-ternary */
 /* eslint-disable func-names */
 const path = require('path');
-// importuję bibliotękę [path] z [node.js]
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-// importuję odpowiedni plugin
 
 module.exports = function (env = {}) {
     const { production: isProd = false } = env;
-    // eslint-disable-next-line
+    // eslint-disable-next-line no-console
     console.log(env);
 
     return {
         entry: './src/app.js',
         mode: isProd ? 'production' : 'development',
         devtool: isProd ? false : 'source-map',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.min.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader',
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: !isProd,
+                            },
+                        },
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif)$/i,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: 'index.html',
+            }),
+            new ESLintPlugin(),
+        ],
     };
 };
-
-module.exports = {
-    entry: './src/app.js',
-    // definiuje plik wejściowy
-    mode: 'development',
-    // definiuję tryb działania
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        // definiuje ścieżką wyjściową
-        filename: 'app.min.js',
-        // definiuję nazwę pliku wyjściowego
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                // określam jakie pliki
-                // będą brane pod uwagę
-                exclude: /node_modules/,
-                // określam wykluczenia
-                use: 'babel-loader',
-                // określam jaki [loader]
-                // ma być wykorzystany
-            },
-            {
-                test: /\.css$/,
-                // tylko pliki z rozszerzeniem .css
-                exclude: /node_modules/,
-                // wykluczam pliki zawierajace
-                // wpisany ciag znakow w sciezce
-                use: ['style-loader', 'css-loader'],
-                // okreslam kolejnosc wykorzystanych
-                // loaderow, tj. od lewej do prawej.
-            },
-            {
-                test: /\.scss$/,
-                // tylko pliki z rozszerzeniem .scss
-                use: [
-                    isProd ? CssWebpackPlugin.loader : 'style-loader',
-                    // 'css-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: isProd ? false : true,
-                        },
-                    },
-                    // 'sass-loader'
-                    // ...
-                ],
-            },
-        ],
-        // obecnie brak dodatkowych ustawień
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            // wskazuje plik źródłowy
-            filename: 'index.html',
-            // określan nazwę dla pliku
-        }),
-        new ESLintPlugin(),
-    ],
-};
-// eksportuję ustawienia dla webpack-a
